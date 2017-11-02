6e3c168a8009e7406cb622694019472cb6aabe3b
oleh <oleh@devlop.loc>
2017-11-02 12:48:17 +0200

    create ftp

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -19,4 +19,12 @@ class AdminDashboardController extends Controller
 			'data'=> '',
 		]);;
 	}
+
+	public function getFile(){
+//		return Storage::disk('ftp')->exists('price1.json')?'true':'false';
+//		return Storage::disk('ftp')->lastModified('price.json');
+		Storage::disk('local')->put('new.file', Storage::disk('ftp')->get('price.json'), 'public');
+		Storage::setVisibility('new.file', 'public');
+		return 'upload';
+	}
 }
