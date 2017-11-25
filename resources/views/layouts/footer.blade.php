9f485f099a4999d5a01d6428c12c236e736d132a
Oleh Levchenko <zloyleva@gmail.com>
2017-11-25 20:49:41 +0200

    add fontawesome

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,26 @@
-    <footer></footer>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-6 section_logo">
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="/images_service/logo.png" alt="">
+                    </a>
+                    <h3 class="logo-description">Самый крупный в Запорожье супермаркет-склад канцелярии, школьной продукции, детских игрушек и новогодних товаров</h3>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-3 section_info">
+                    <h4>Information</h4>
+                </div>
+                <div class="col-sm-3 col-md-3 col-lg-3 section_contacts">
+                    <h4>Contacts</h4>
+                    <div>г. Запорожье,</div>
+                    <div>ул. Деповская 72</div>
+                    <div><a href="tel:0617699545"><i class="fa fa-phone" aria-hidden="true"></i> (061) 769 95 45 /c 8:30 до 19:00/</a></div>
+                    <div><a href="tel:0676180545"><i class="fa fa-mobile" aria-hidden="true"></i> (067) 618 05 45 /без выходных/</a></div>
+                </div>
+            </div>
+        </div>
+    </footer>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
