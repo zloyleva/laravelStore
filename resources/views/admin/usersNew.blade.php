948fe28aa4d5dfe05b4c5aa642677966b1989196
Oleh Levchenko <zloyleva@gmail.com>
2017-12-18 21:06:24 +0200

    hidde edit email

--- resources/views/admin/usersNew.blade.php
+++ resources/views/admin/usersNew.blade.php
@@ -9,9 +9,7 @@
             Add new user
         @endif
     </h1>
-    @if(isset($user))
-        <a href="{{route('admin.users.index')}}" class="btn btn-success">Back</a>
-    @endif
+    <a href="{{route('admin.users.index')}}" class="btn btn-success">Back</a>
 </div>
 <div class="container">
     <div class="row">
@@ -31,20 +29,19 @@
                     <label for="InputLName">Last name</label>
                     <input name="lname" type="text" class="form-control" id="InputLName" placeholder="Enter last name" value="{{ $user->lname or ''  }}">
                 </div>
-                <div class="form-group">
+                <div class="form-group ">
                     <label for="InputEmail">Email</label>
-                    <input name="email" type="email" class="form-control
-                            @if(isset($user))
-                                hidden
-                            @endif
-                           " id="InputEmail" placeholder="Enter email"
-                           value="{{ $user->email or ''  }}">
+                    <input name="email" type="email" class="form-control {{ isset($user)?'hidden':'' }}"
+                           id="InputEmail" placeholder="Enter email" value="{{ $user->email or ''  }}">
+                    @if( isset($user) )
+                        <p>{{ $user->email or ''  }}</p>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="InputPassword">Password</label>
                     <input name="password" type="text" class="form-control" id="InputPassword" placeholder="Enter password">
                 </div>
-                <div class="form-group">
+                <div class="form-group {{ isset($user)?'hidden':'' }}">
                     <label for="InputRole">Role</label>
                     <select name="role" class="form-control" id="InputRole" disabled>
                         <option value="buyer" selected>buyer</option>
@@ -55,11 +52,9 @@
                     <label for="InputPrice">Price type</label>
                     <select name="price_type" class="form-control" id="InputPrice">
                         @foreach($priceTypeList as $item)
-                            <option value="{{$item->id}}"
-                            @if(isset($user) && $user->price_type == $item->id)
-                                selected
-                            @endif
-                            >{{$item->description}}</option>
+                            <option value="{{$item->id}}" {{ (isset($user) && $user->price_type == $item->id)?'selected':'' }}>
+                                {{$item->description}}
+                            </option>
                         @endforeach
                     </select>
                 </div>
@@ -77,21 +72,15 @@
                     <label for="InputPrice">Manager</label>
                     <select name="manager_id" class="form-control" id="InputPrice">
                         @foreach($managerList as $manager)
-                            <option value="{{$manager->id}}"
-                            @if(isset($user) && $user->manager_id == $manager->id)
-                            selected
-                            @endif
-                            >{{$manager->name}}</option>
+                            <option value="{{$manager->id}}" {{ (isset($user) && $user->manager_id == $manager->id)?'selected':'' }}>
+                                {{$manager->name}}
+                            </option>
                         @endforeach
                     </select>
                 </div>
                 <div class="form-group">
                     <button id="addUser" class="btn btn-primary">
-                        @if(isset($user))
-                            Update user
-                        @else
-                            Add user
-                        @endif
+                        {{ (isset($user))?'Update user':'Add user' }}
                     </button>
                 </div>
             </form>
