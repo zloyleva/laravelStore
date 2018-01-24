c480d65f72761f3101a732c73add6a2364862d76
Oleh Levchenko <zloyleva@gmail.com>
2018-01-24 22:41:37 +0200

    add to products link for own category

--- resources/views/product/section/product-info.blade.php
+++ resources/views/product/section/product-info.blade.php
@@ -1,3 +1,3 @@
-<div class="product_category">Категория: {{$product->category->name}}</div>
+<div class="product_category">Категория: <a href="/store/category/{{$product->category->slug}}">{{$product->category->name}}</a></div>
 <div class="product_sku">Артикул: {{$product->sku}}</div>
 <div class="product_stock">Количество: {{$product->stock}}</div>
