6fbf5ea1da6546e2c66e43ff7ed84adf895f2cda
zloyleva <zloyleva@gmail.com>
2018-07-27 22:49:36 +0300

    set JSON_UNESCAPED_UNICODE for controller json response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,15 +11,16 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-	/**
-	 * @param array $defaultResponse
-	 * @param int $status
-	 *
-	 * @return \Illuminate\Http\JsonResponse
-	 */
-    protected function jsonResponse($defaultResponse = ['message' => 'Done.'], $status = 200)
+    /**
+     * @param array $defaultResponse
+     * @param int $status
+     * @param array $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function jsonResponse($defaultResponse = ['message' => 'Done.'], $status = 200, $headers=[])
     {
-        return response()->json($defaultResponse, $status);
+        $headers = array_merge($headers, [ 'Content-Type' => 'application/json; charset=utf-8' ]);
+        return response()->json($defaultResponse, $status, $headers, JSON_UNESCAPED_UNICODE);
     }
 
 }
