80816b28a2d55519b0f0630feff9c8ff3a632a50
zloyleva <zloyleva@gmail.com>
2018-08-26 23:31:37 +0300

    close first_time modal

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -84,19 +84,19 @@
 
     @if( !isset($_COOKIE['firsttime']) )
         <?php setcookie("firsttime", "no", 0); ?>
-        <div class="modal first_time" tabindex="-1" role="dialog" style="display: block">
+        <div id="first_time" class="modal first_time" tabindex="-1" role="dialog" style="display: block">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <button type="button" class="close close_first_time" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <h4 class="modal-title">Привет, гость!</h4>
                     </div>
                     <div class="modal-body">
                         <p>Для того чтобы получить оптовую цену на товары, Вам нужно зарегистрироваться в нашем магазине</p>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
-                        <a href="#" type="button" class="btn btn-primary">Зарегистрироваться</a>
+                        <button type="button" class="btn btn-default close_first_time" data-dismiss="modal">Закрыть</button>
+                        <a href="/register" type="button" class="btn btn-primary">Зарегистрироваться</a>
                     </div>
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
